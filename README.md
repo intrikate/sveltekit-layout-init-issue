7b6c61dd5d10a476c3526997bf5b434066b8afc2
Kate Lee <kate@katelee.studio>
2021-04-25 07:33:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SvelteKit `$layout` initialisation issue
+In development, updating `$layout.svelte` and reloading the page makes it initialise and mount twice: once on reload and once on subsequent navigation.
---
+++ src/routes/$layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import {onMount, onDestroy} from "svelte";
+
+	console.log("Layout init"); //
+	onMount(() => console.log("Layout mount"));
+	onDestroy(() => console.log("Layout unmount"));
+</script>
+
+<a href="/">Home</a> <a href="/other">Other</a>
+
+<slot />
